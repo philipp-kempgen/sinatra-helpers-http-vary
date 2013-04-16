6559b9b83f464c389257dbf2d90100bb251702ce
Philipp Kempgen <lists@kempgen.net>
2013-04-16 15:55:19 +0200

    cleanup

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
-Sinatra helpers to set the HTTP `Vary` header.
+Sinatra helpers to set the HTTP "`Vary`" header.
 
 See [RFC 2616 (HTTP/1.1): Vary](http://tools.ietf.org/html/rfc2616#section-14.44)
 
 Author: Philipp Kempgen, [http://kempgen.net](http://kempgen.net)
+
+
+## Usage
+
+Include the module in your Sinatra application:
+
+	helpers ::Sinatra::Helpers::HTTP::Vary
+
+Example:
+
+	vary! 'Accept'
+	vary! 'Accept-Language'
+	# headers['Vary'] is now "Accept, Accept-Language"
+
+Example:
+
+	vary! '*'
+	# headers['Vary'] is now "*"
+
+Example:
+
+	vary! '*'
+	vary! 'Accept-Language'
+	# headers['Vary'] is now "*"
+
+Example:
+
+	vary! 'Accept-Language'
+	vary! '*'
+	# headers['Vary'] is now "*"
 
--- lib/sinatra-helpers/http/vary.rb
+++ lib/sinatra-helpers/http/vary.rb
@@ -1,37 +1,67 @@
 require 'sinatra/base'
 
+# see Sinatra
 module Sinatra
+	# see Sinatra
 	module Helpers
-		module Http
+		
+		# HTTP helpers
+		module HTTP
 			
-			def vary( header_name )
-				if header_name.to_s == '*'
-					headers['Vary'] = '*'
-				else
-					vary_headers = headers['Vary'].to_s.
-						split( /\s*(?:,\s*)+/ )
+			# Helpers to set the HTTP "`Vary`" header.
+			#
+			# Usage:
+			#
+			# Include the module in your Sinatra application:
+			#
+			#   helpers ::Sinatra::Helpers::HTTP::Vary
+			#
+			module Vary
+				
+				VARY_HEADER = 'Vary'.freeze
+				VARY_UNSPECIFIED = '*'.freeze
+				
+				# Sets the HTTP "`Vary`" header in Sinatra's response
+				# `headers`.
+				#
+				# @param   hdr_name  [String]   The HTTP header name.
+				# @return                       The updated `headers`.
+				#
+				def vary!( hdr_name )
+					hdr_name = hdr_name.to_s  if ! hdr_name.kind_of?( ::String )
 					
-					if vary_headers.include?( '*' )
-						headers['Vary'] = '*'
+					# Shortcut to avoid expensive splitting in the
+					# simple "*" case:
+					if hdr_name == VARY_UNSPECIFIED
+						headers[ VARY_HEADER ] = VARY_UNSPECIFIED.dup  unless (headers[ VARY_HEADER ] == VARY_UNSPECIFIED)
+						
+					# Normal operation:
 					else
-						headers['Vary'] =
-							vary_headers.
-							push( header_name.to_s ).
-							uniq( & :downcase ).
-							join( ',' )
+						vary_hdrs = headers[ VARY_HEADER ].to_s.
+							split( /\s*(?:,\s*)+/ )
+						
+						if vary_hdrs.include?( VARY_UNSPECIFIED )
+							headers[ VARY_HEADER ] = VARY_UNSPECIFIED.dup
+						else
+							headers[ VARY_HEADER ] =
+								vary_hdrs.
+								push( hdr_name ).
+								uniq( & :downcase ).
+								join( ',' )
+						end
 					end
+					return headers[ VARY_HEADER ]
 				end
-				return headers['Vary']
+				
+				#def vary=( str )
+				#	headers[ VARY_HEADER ] = str
+				#end
+				
 			end
-			
-			#def vary=( str )
-			#	headers['Vary'] = str
-			#end
-			
 		end
 	end
 	
-	#helpers Helpers::Http
-	#::Sinatra::Base.helpers Helpers::Http
+	#helpers Helpers::HTTP::Vary
+	#::Sinatra::Base.helpers Helpers::HTTP::Vary
 end
 
